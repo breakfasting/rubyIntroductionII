87ad6f2c1b30cf77e3bde55f4a4eb7c95fd5ecff
breakfasting <breakfast.ing@gmail.com>
2020-01-09 23:31:43 +0800

    watch out for infinite loops

---
+++ TechInterviewII/pyramid_sum.rb
@@ -0,0 +1,31 @@
+# For example, the base [1, 4, 6] gives us the following pyramid
+#     15
+#   5   10
+# 1   4    6
+
+def pyramid_sum(base)
+    # while the array length isn't 1
+    # iterate through the level to sum the next_level
+    # unshift the next level to the answer array
+    pyramid = []
+    pyramid.unshift(base) #[[1, 4, 6]]
+
+   
+    while base.length > 1
+        i = 1
+        next_level = []
+        while i < base.length
+            next_level << base [i] + base[i - 1] # [5, 10]
+            i += 1
+        end
+        pyramid.unshift(next_level)
+        base = next_level
+    end
+    return pyramid
+end
+
+print pyramid_sum([1, 4, 6]) #=> [[15], [5, 10], [1, 4, 6]]
+puts
+
+print pyramid_sum([3, 7, 2, 11]) #=> [[41], [19, 22], [10, 9, 13], [3, 7, 2, 11]]
+puts
