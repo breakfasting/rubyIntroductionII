adf86d2b581354587ce8f63f84670766b3dd7cd5
breakfasting <breakfast.ing@gmail.com>
2020-01-10 05:03:29 +0800

    not a while loop

--- TechInterviewII/pyramid_sum.rb
+++ TechInterviewII/pyramid_sum.rb
@@ -13,11 +13,17 @@ def pyramid_sum(base)
    
     while base.length > 1
         i = 1
+        # next_level = []
+        # while i < base.length
+        #     next_level << base [i] + base[i - 1] # [5, 10]
+        #     i += 1
+        # end
+
         next_level = []
-        while i < base.length
-            next_level << base [i] + base[i - 1] # [5, 10]
-            i += 1
+        (1...base.length).each do |i|
+            next_level << base [i] + base[i - 1]
         end
+
         pyramid.unshift(next_level)
         base = next_level
     end
