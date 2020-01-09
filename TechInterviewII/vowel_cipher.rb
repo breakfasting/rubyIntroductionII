58fe0906cc5ae209cf6c56ce226f76f3beb5f9ff
breakfasting <breakfast.ing@gmail.com>
2020-01-10 05:30:54 +0800

    hash map version

--- TechInterviewII/vowel_cipher.rb
+++ TechInterviewII/vowel_cipher.rb
@@ -1,17 +1,35 @@
 def vowel_cipher(string)
   #iterate through the string
   #change each vowel to next one
-  vowel = "aeiou"
-  new_word = ""
-  string.each_char.with_index do |char, i| # bootcamp
-    vowel_i = vowel.index(char)
-    if  vowel_i != nil
-      new_word += vowel[(vowel_i + 1) % 5]
-    else
-      new_word += char
+#   vowel = "aeiou"
+#   new_word = ""
+#   string.each_char.with_index do |char, i| # bootcamp
+#     vowel_i = vowel.index(char)
+#     if  vowel_i != nil
+#       new_word += vowel[(vowel_i + 1) % 5]
+#     else
+#       new_word += char
+#     end
+#   end
+#   return new_word
+
+    vowel = {
+        "a" => "e",
+        "e" => "i",
+        "i" => "o",
+        "o" => "u",
+        "u" => "a"
+    }
+    new_str = ""
+    string.each_char do |char|
+        if vowel[char] != nil
+            new_str += vowel[char]
+        else
+            new_str += char
+        end
     end
-  end
-  return new_word
+    return new_str
+
 end
 
 puts vowel_cipher("bootcamp") #=> buutcemp
