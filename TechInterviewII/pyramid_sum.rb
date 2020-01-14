24705e0d86b924e838a10bd3221cdfe6afdf380a
breakfasting <breakfast.ing@gmail.com>
2020-01-14 14:19:37 +0800

    fixing spaces

--- TechInterviewII/pyramid_sum.rb
+++ TechInterviewII/pyramid_sum.rb
@@ -4,30 +4,29 @@
 # 1   4    6
 
 def pyramid_sum(base)
-    # while the array length isn't 1
-    # iterate through the level to sum the next_level
-    # unshift the next level to the answer array
-    pyramid = []
-    pyramid.unshift(base) #[[1, 4, 6]]
+  # while the array length isn't 1
+  # iterate through the level to sum the next_level
+  # unshift the next level to the answer array
+  pyramid = []
+  pyramid.unshift(base) #[[1, 4, 6]]
 
-   
-    while base.length > 1
-        i = 1
-        # next_level = []
-        # while i < base.length
-        #     next_level << base [i] + base[i - 1] # [5, 10]
-        #     i += 1
-        # end
+  while base.length > 1
+    i = 1
+    # next_level = []
+    # while i < base.length
+    #     next_level << base [i] + base[i - 1] # [5, 10]
+    #     i += 1
+    # end
 
-        next_level = []
-        (1...base.length).each do |i|
-            next_level << base [i] + base[i - 1]
-        end
-
-        pyramid.unshift(next_level)
-        base = next_level
+    next_level = []
+    (1...base.length).each do |i|
+      next_level << base[i] + base[i - 1]
     end
-    return pyramid
+
+    pyramid.unshift(next_level)
+    base = next_level
+  end
+  return pyramid
 end
 
 print pyramid_sum([1, 4, 6]) #=> [[15], [5, 10], [1, 4, 6]]
